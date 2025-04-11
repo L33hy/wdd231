a9a0f35643e4b538a87daf79788802505f1c2f01
L33hy <lehidaniel99@gmail.com>
2025-04-11 15:58:46 +0100

    Adjusted the html

--- chamber/scripts/general.js
+++ chamber/scripts/general.js
@@ -1,19 +1,21 @@
-document.addEventListener("DOMContentLoaded", function () {
-	document.getElementById("year").textContent = new Date().getFullYear();
+document.addEventListener('DOMContentLoaded', function () {
+	document.getElementById('year').textContent = new Date().getFullYear();
 
-	document.getElementById("lastModified").textContent =
+	document.getElementById('lastModified').textContent =
 		"Last modification: " + document.lastModified;
 
 	// Hamburger menu toggle
-	var hamburger = document.getElementById("hamburger");
-	var navMenu = document.getElementById("nav-menu");
+	const hamburger = document.querySelector('#hamburger');
+	const navMenu = document.querySelector('#nav-links');
 
-	hamburger.addEventListener("click", function () {
-		navMenu.classList.toggle("show");
-		if (hamburger.textContent === "✕") {
-			hamburger.textContent = "☰";
-		} else {
-			hamburger.textContent = "✕";
-		}
+	hamburger.addEventListener('click', function () {
+		navMenu.classList.toggle('open');
+		if (navbar.classList.contains('open')) {
+            // navbar.classList.add('open');
+            hamburger.textContent = 'X';
+        } else {
+            hamburger.textContent = '☰';
+            // navbar.classList.remove('open');
+        }
 	});
 });
